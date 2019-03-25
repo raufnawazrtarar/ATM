bf1480e384638e4763b3338006f749a6e4f612d2
raufnawazrtarar <33519174+raufnawazrtarar@users.noreply.github.com>
2019-03-25 10:40:01 +0000

    Update ATM.Designer.cs

--- ATM/ATM/ATM.Designer.cs
+++ ATM/ATM/ATM.Designer.cs
@@ -31,16 +31,31 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numOneBtn = new System.Windows.Forms.Button();
+            this.numTwoBtn = new System.Windows.Forms.Button();
+            this.numThreeBtn = new System.Windows.Forms.Button();
+            this.numFourBtn = new System.Windows.Forms.Button();
+            this.numFiveBtn = new System.Windows.Forms.Button();
+            this.numSixBtn = new System.Windows.Forms.Button();
+            this.numSevenBtn = new System.Windows.Forms.Button();
+            this.numEightBtn = new System.Windows.Forms.Button();
+            this.numNineBtn = new System.Windows.Forms.Button();
+            this.numZeroBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.enterBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImage = global::ATM.Properties.Resources.BlueScreenImg;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(200, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(400, 300);
+            this.pictureBox1.Size = new System.Drawing.Size(528, 339);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -48,7 +63,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(226, 133);
+            this.label1.Location = new System.Drawing.Point(73, 57);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(351, 20);
             this.label1.TabIndex = 1;
@@ -57,17 +72,155 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(311, 180);
+            this.textBox1.Location = new System.Drawing.Point(174, 120);
             this.textBox1.MaxLength = 6;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(199, 22);
             this.textBox1.TabIndex = 2;
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.enterBtn);
+            this.panel1.Controls.Add(this.clearBtn);
+            this.panel1.Controls.Add(this.cancelBtn);
+            this.panel1.Controls.Add(this.numZeroBtn);
+            this.panel1.Controls.Add(this.numNineBtn);
+            this.panel1.Controls.Add(this.numEightBtn);
+            this.panel1.Controls.Add(this.numSevenBtn);
+            this.panel1.Controls.Add(this.numSixBtn);
+            this.panel1.Controls.Add(this.numFiveBtn);
+            this.panel1.Controls.Add(this.numFourBtn);
+            this.panel1.Controls.Add(this.numThreeBtn);
+            this.panel1.Controls.Add(this.numTwoBtn);
+            this.panel1.Controls.Add(this.numOneBtn);
+            this.panel1.Location = new System.Drawing.Point(12, 344);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(528, 284);
+            this.panel1.TabIndex = 3;
+            // 
+            // numOneBtn
+            // 
+            this.numOneBtn.Location = new System.Drawing.Point(0, 0);
+            this.numOneBtn.Name = "numOneBtn";
+            this.numOneBtn.Size = new System.Drawing.Size(115, 66);
+            this.numOneBtn.TabIndex = 0;
+            this.numOneBtn.Text = "1";
+            this.numOneBtn.UseVisualStyleBackColor = true;
+            // 
+            // numTwoBtn
+            // 
+            this.numTwoBtn.Location = new System.Drawing.Point(125, 0);
+            this.numTwoBtn.Name = "numTwoBtn";
+            this.numTwoBtn.Size = new System.Drawing.Size(115, 66);
+            this.numTwoBtn.TabIndex = 1;
+            this.numTwoBtn.Text = "2";
+            this.numTwoBtn.UseVisualStyleBackColor = true;
+            // 
+            // numThreeBtn
+            // 
+            this.numThreeBtn.Location = new System.Drawing.Point(246, 0);
+            this.numThreeBtn.Name = "numThreeBtn";
+            this.numThreeBtn.Size = new System.Drawing.Size(115, 66);
+            this.numThreeBtn.TabIndex = 2;
+            this.numThreeBtn.Text = "3";
+            this.numThreeBtn.UseVisualStyleBackColor = true;
+            // 
+            // numFourBtn
+            // 
+            this.numFourBtn.Location = new System.Drawing.Point(0, 72);
+            this.numFourBtn.Name = "numFourBtn";
+            this.numFourBtn.Size = new System.Drawing.Size(115, 66);
+            this.numFourBtn.TabIndex = 3;
+            this.numFourBtn.Text = "4";
+            this.numFourBtn.UseVisualStyleBackColor = true;
+            // 
+            // numFiveBtn
+            // 
+            this.numFiveBtn.Location = new System.Drawing.Point(121, 72);
+            this.numFiveBtn.Name = "numFiveBtn";
+            this.numFiveBtn.Size = new System.Drawing.Size(115, 66);
+            this.numFiveBtn.TabIndex = 4;
+            this.numFiveBtn.Text = "5";
+            this.numFiveBtn.UseVisualStyleBackColor = true;
+            // 
+            // numSixBtn
+            // 
+            this.numSixBtn.Location = new System.Drawing.Point(246, 72);
+            this.numSixBtn.Name = "numSixBtn";
+            this.numSixBtn.Size = new System.Drawing.Size(115, 66);
+            this.numSixBtn.TabIndex = 5;
+            this.numSixBtn.Text = "6";
+            this.numSixBtn.UseVisualStyleBackColor = true;
+            // 
+            // numSevenBtn
+            // 
+            this.numSevenBtn.Location = new System.Drawing.Point(0, 144);
+            this.numSevenBtn.Name = "numSevenBtn";
+            this.numSevenBtn.Size = new System.Drawing.Size(115, 66);
+            this.numSevenBtn.TabIndex = 6;
+            this.numSevenBtn.Text = "7";
+            this.numSevenBtn.UseVisualStyleBackColor = true;
+            // 
+            // numEightBtn
+            // 
+            this.numEightBtn.Location = new System.Drawing.Point(121, 144);
+            this.numEightBtn.Name = "numEightBtn";
+            this.numEightBtn.Size = new System.Drawing.Size(115, 66);
+            this.numEightBtn.TabIndex = 7;
+            this.numEightBtn.Text = "8";
+            this.numEightBtn.UseVisualStyleBackColor = true;
+            // 
+            // numNineBtn
+            // 
+            this.numNineBtn.Location = new System.Drawing.Point(246, 144);
+            this.numNineBtn.Name = "numNineBtn";
+            this.numNineBtn.Size = new System.Drawing.Size(115, 66);
+            this.numNineBtn.TabIndex = 8;
+            this.numNineBtn.Text = "9";
+            this.numNineBtn.UseVisualStyleBackColor = true;
+            // 
+            // numZeroBtn
+            // 
+            this.numZeroBtn.Location = new System.Drawing.Point(121, 216);
+            this.numZeroBtn.Name = "numZeroBtn";
+            this.numZeroBtn.Size = new System.Drawing.Size(115, 66);
+            this.numZeroBtn.TabIndex = 9;
+            this.numZeroBtn.Text = "0";
+            this.numZeroBtn.UseVisualStyleBackColor = true;
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(410, 3);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(115, 66);
+            this.cancelBtn.TabIndex = 10;
+            this.cancelBtn.Text = "CANCEL";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(410, 75);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(115, 66);
+            this.clearBtn.TabIndex = 11;
+            this.clearBtn.Text = "CLEAR";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            // 
+            // enterBtn
+            // 
+            this.enterBtn.Location = new System.Drawing.Point(410, 147);
+            this.enterBtn.Name = "enterBtn";
+            this.enterBtn.Size = new System.Drawing.Size(115, 66);
+            this.enterBtn.TabIndex = 12;
+            this.enterBtn.Text = "ENTER";
+            this.enterBtn.UseVisualStyleBackColor = true;
+            // 
             // ATM
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(782, 753);
+            this.ClientSize = new System.Drawing.Size(551, 630);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
@@ -75,6 +228,7 @@
             this.Text = "ATM";
             this.Load += new System.EventHandler(this.ATM_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,5 +239,19 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button enterBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.Button numZeroBtn;
+        private System.Windows.Forms.Button numNineBtn;
+        private System.Windows.Forms.Button numEightBtn;
+        private System.Windows.Forms.Button numSevenBtn;
+        private System.Windows.Forms.Button numSixBtn;
+        private System.Windows.Forms.Button numFiveBtn;
+        private System.Windows.Forms.Button numFourBtn;
+        private System.Windows.Forms.Button numThreeBtn;
+        private System.Windows.Forms.Button numTwoBtn;
+        private System.Windows.Forms.Button numOneBtn;
     }
 }
