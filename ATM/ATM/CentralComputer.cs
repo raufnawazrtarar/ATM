34bb54176f1595af64117e8109143c68de5a5751
ArchieRutherford <33519215+ArchieRutherford@users.noreply.github.com>
2019-03-26 11:19:41 +0000

    Added Semaphore

--- ATM/ATM/CentralComputer.cs
+++ ATM/ATM/CentralComputer.cs
@@ -50,6 +50,7 @@ namespace ATM
         private int balance;
         private int pin;
         private int accountNum;
+        public Semaphore accessed;
 
         // a constructor that takes initial values for each of the attributes (balance, pin, accountNumber)
         public Account(int balance, int pin, int accountNum)
@@ -57,6 +58,7 @@ namespace ATM
             this.balance = balance;
             this.pin = pin;
             this.accountNum = accountNum;
+            accessed = new Semaphore(0, 1);
         }
 
         //getter and setter functions for balance
